b4dbb9d1e054cdb1d297dd60a17aa5d28ad1393d
JHON SOSA <sjhonathan@gmail.com>
2024-05-16 11:55:59 -0400

    se corrigio bug para tabla movimiento pueda eliminar masivamente  1.0.21

--- app/Livewire/MovimientoTable.php
+++ app/Livewire/MovimientoTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Exports\MovimientosExport;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Movimiento;
+use Maatwebsite\Excel\Facades\Excel;
 
 class MovimientoTable extends DataTableComponent
 {
@@ -104,5 +106,28 @@ class MovimientoTable extends DataTableComponent
                     'id'=> $row->id
                 ])),
         ];
+    }
+    public  function deleteSelected(){
+        if ($this->getSelected()) {
+            $movimientos= Movimiento::whereIn('id', $this->getSelected())->delete();
+            
+            $this->clearSelected();
+            // $this->emit('Atencion', 'El registro fue eliminado correctamente');
+
+            
+        }else {
+            $this->emit('error', 'NO hay registro seleccionado');
+        }
+
+    }
+    public function exportSelected(){
+        if ($this->getSelected()) {
+            
+            $movimientos= Movimiento::whereIn('id', $this->getSelected())->get();
+            $this->clearSelected();
+            return Excel::download(new MovimientosExport($movimientos), 'movimientos.xlsx');   
+        }else {
+           // return Excel::download(new ClientesExport($this->getRows()), 'clientes.xlsx');
+        }
     }
 }
