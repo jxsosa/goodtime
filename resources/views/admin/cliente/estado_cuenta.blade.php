9b5768a63e7f1b754c1a3e7a8a07334d1b9ece9c
JHON SOSA <sjhonathan@gmail.com>
2024-06-05 13:34:47 -0400

    se corrigio como se muetra los valores de monto 1.0.62

--- resources/views/admin/cliente/estado_cuenta.blade.php
+++ resources/views/admin/cliente/estado_cuenta.blade.php
@@ -1,75 +1,74 @@
 @php
-  use App\Models\Movimiento;  
-    
-//   $sumaMontosSalida = Movimiento::where('tipo', 'salida')
-//     ->where('cliente_id', $cliente->id) // Reemplaza 'cliente_id' con el nombre correcto de la columna
-//     ->sum('monto');
-// $sumaMontosEntreda = Movimiento::where('tipo', 'entrada')
-//     ->where('cliente_id', $cliente->id) // Reemplaza 'cliente_id' con el nombre correcto de la columna
-//     ->sum('monto');
+    use App\Models\Movimiento;
 
+    //   $sumaMontosSalida = Movimiento::where('tipo', 'salida')
+    //     ->where('cliente_id', $cliente->id) // Reemplaza 'cliente_id' con el nombre correcto de la columna
+    //     ->sum('monto');
+    // $sumaMontosEntreda = Movimiento::where('tipo', 'entrada')
+    //     ->where('cliente_id', $cliente->id) // Reemplaza 'cliente_id' con el nombre correcto de la columna
+    //     ->sum('monto');
 
+    $saldo = 0;
 
+    foreach ($movimientos as $movimiento) {
+        if ($movimiento->tipo === 'salida') {
+            $saldo -= $movimiento->monto;
+        } else {
+            $saldo += $movimiento->monto;
+        }
 
- $saldo = 0;
-
-
-foreach ($movimientos as $movimiento) {
-    if ($movimiento->tipo === 'salida') {
-        $saldo -= $movimiento->monto;
-    } else {
-        $saldo += $movimiento->monto;
+        // Agrega el saldo al objeto de movimiento
+        $movimiento->saldo = $saldo;
     }
 
-    // Agrega el saldo al objeto de movimiento
-    $movimiento->saldo = $saldo;
-}
-
-
-// $saldo=$sumaMontosEntreda-$sumaMontosSalida;
-$saldo=$movimiento->saldo = $saldo;
-;
-$MontoSaldo=0;   
+    // $saldo=$sumaMontosEntreda-$sumaMontosSalida;
+    $saldo = $movimiento->saldo = $saldo;
+    $MontoSaldo = 0;
 @endphp
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-   
+
     <title>Estado de Cuenta</title>
-    
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-     <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
+
 <body>
-<div>
     <div>
-        
-                <img src="vendor/adminlte/dist/img/logo.png" alt="Logo de Gootime" width="80" height="80">
-           
-    </div>
-    <div class="text-rigth">
-    <p class="text-uppercase"><b>CLIENTE:</b> {{ $cliente->nombre }} <br>
-    @if ($saldo<'0')
-    <h3><span class="badge badge-danger"> <b> SALDO:</b> ${{number_format($saldo, 2, '.', ',')  }}</span></h3>
-    @else
-    <h3><span class="badge badge-success"> <b> SALDO:</b> ${{number_format($saldo, 2, '.', ',')  }}</span></h3>
-    @endif
-        
-    </p>
-   
+        <div>
+
+            <img src="vendor/adminlte/dist/img/logo.png" alt="Logo de Gootime" width="80" height="80">
+
+        </div>
+        <div class="text-rigth">
+            <p class="text-uppercase"><b>CLIENTE:</b> {{ $cliente->nombre }} <br>
+                @if ($saldo < '0')
+                    <h3><span class="badge badge-danger"> <b> SALDO:</b>
+                            ${{ number_format($saldo, 2, '.', ',') }}</span></h3>
+                @else
+                    <h3><span class="badge badge-success"> <b> SALDO:</b>
+                            ${{ number_format($saldo, 2, '.', ',') }}</span></h3>
+                @endif
+
+            </p>
+
+        </div>
+
+
     </div>
 
 
-</div>
-    
-    
     <h1 class="text-center">Estado de Cuenta</h1>
-    
+
     <table class="table  table-bordered table-sm table-striped">
         <thead class="table-dark text-center">
             <tr>
@@ -82,38 +81,41 @@ $MontoSaldo=0;
                 <th>Saldo</th>
             </tr>
         </thead>
-        <tbody >
+        <tbody>
             @foreach ($movimientos as $movimiento)
                 <tr>
-                    <td class="small"><small>{{ $movimiento->created_at->format('d/m/y h:m')}}</small></td>
+                    <td class="small"><small>{{ $movimiento->created_at->format('d/m/y h:m') }}</small></td>
                     <td class="small"><small>{{ $movimiento->descripcion }}</small></td>
                     <td class="small"><small>{{ $movimiento->cuenta->nombre }}</small></td>
-                    <td class="text-center">{{ number_format($movimiento->bs, 2, '.', ',')}}</td>
-                    <td class="text-center">{{ number_format($movimiento->tasa, 2, '.', ',')}}</td>
-                    <td class="text-center">${{$monto = ($movimiento->tipo==='entrada') ? number_format($movimiento->monto, 2, '.', ',') : number_format($movimiento->monto*-1, 2, '.', ',') ;  }}</td>
+                    <td class="text-center"><small>{{ number_format($movimiento->bs, 2, '.', ',') }}</small></td>
+                    <td class="text-center"><small>{{ number_format($movimiento->tasa, 2, '.', ',') }}</small></td>
+                    <td class="text-center">
+                        <small>${{ $monto = $movimiento->tipo === 'entrada' ? number_format($movimiento->monto, 2, '.', ',') : number_format($movimiento->monto * -1, 2, '.', ',') }}</small>
+                    </td>
                     {{-- <td class="text-center">$ {{$movimiento->monto }}</td> --}}
                     {{-- <td class="text-center">${{ $MontoSaldo=$MontoSaldo+ $monto}}</td> --}}
                     @if ($loop->first)
-        {{-- Esto es la primera iteración --}}
-        <td class="text-center">${{ number_format($saldo, 2, '.', ',')}}</td>
-        @php
-        $montoFormateado = (float) str_replace(',', '', $monto);
-         
-          $saldo=$saldo-$montoFormateado;
-        @endphp
-        @else
-        <td class="text-center">${{ number_format($saldo, 2, '.', ',')}}</td>
-         @php
-        //  $monto = number_format($monto, 2); // Formatea el valor con 2 decimales
-        $montoFormateado = (float) str_replace(',', '', $monto);  
-         $saldo=$saldo-$montoFormateado;
-        @endphp
-        @endif
-       
-                    
+                        {{-- Esto es la primera iteración --}}
+                        <td class="text-center">${{ number_format($saldo, 2, '.', ',') }}</td>
+                        @php
+                            $montoFormateado = (float) str_replace(',', '', $monto);
+
+                            $saldo = $saldo - $montoFormateado;
+                        @endphp
+                    @else
+                        <td class="text-center">${{ number_format($saldo, 2, '.', ',') }}</td>
+                        @php
+                            //  $monto = number_format($monto, 2); // Formatea el valor con 2 decimales
+                            $montoFormateado = (float) str_replace(',', '', $monto);
+                            $saldo = $saldo - $montoFormateado;
+                        @endphp
+                    @endif
+
+
                 </tr>
             @endforeach
         </tbody>
     </table>
 </body>
+
 </html>
