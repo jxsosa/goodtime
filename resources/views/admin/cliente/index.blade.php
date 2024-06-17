a51226af1907c159d1e0441e020170f1bbe71438
jhonathan sosa <sjhonathan@gmail.com>
2024-06-17 12:08:54 -0400

    se agrego boton para estado de cuenta en listado de cliente 1.0.69

--- resources/views/admin/cliente/index.blade.php
+++ resources/views/admin/cliente/index.blade.php
@@ -101,44 +101,7 @@
     </div>
     <div class="card">
 
-        {{-- <div class="card-header">
-           
-        </div> --}}
-        {{-- <div class="card-body">
-           <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Telefono</th>
-                    <th colspan="2"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($clientes as $cliente)
-                    <tr>
-                        <td>{{$cliente->id}}</td>
-                        <td><a href="{{route('admin.cliente.show', $cliente)}}">{{$cliente->nombre}}</a></td>
-                        <td>{{$cliente->email}}</td>
-                        <td>{{$cliente->telefono}}</td>
-                        <td width="10px">
-                            <a class="btn btn-primary btn-sm" href="{{route('admin.cliente.edit', $cliente)}}">Editar</a>
-                        </td>
-                        <td width="10px">
-                            <form action="{{route('admin.cliente.destroy', $cliente)}}" method="POST">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>                                
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-
-            </tbody>
-
-           </table>
-        </div> --}}
+        
         <div class="card-body">@livewire('cliente-table')</div>
 
     </div>
--- resources/views/admin/cliente/tables/action.blade.php
+++ resources/views/admin/cliente/tables/action.blade.php
@@ -1,4 +1,5 @@
 <div class="flex d-inline p-3 ">
+    
     <a href="{{route('admin.cliente.show', [
         'cliente'=>$id,
     ])}}" class="btn btn-info btn-sm">Ver</a>
@@ -6,6 +7,8 @@
     <a href="{{route('admin.cliente.edit', [
         'cliente'=>$id,
     ])}}" class="btn btn-primary btn-sm" >Editar</a>
+    <a href="{{ route('admin.cliente.estado_cuenta', ['cliente' => $id]) }}" target="_blank" class="btn btn-sm btn-outline-danger">
+        <i class="glyphicon glyphicon-file"></i>PDF</a>
 
     <form class="d-inline p-3" action="{{route('admin.cliente.destroy', [
         'cliente'=>$id,
